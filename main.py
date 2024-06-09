1fb17174c72acc2ea6eb97b4a2a39523f519ad9a
junhee <wjunhee102@gmail.com>
2024-06-09 15:14:46 +0900

    [feat] 상호작용 기능 추가  - 진행 상황 표시  - 중간에 escape 기능 추가  - excel 파일 설정 추가

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pandas as pd
 import shutil
 import win32com.client as win32
 import os
+from tqdm import tqdm
+import keyboard
 
 def sanitize_name(name):
     invalid_chars = '<>:"/\\|?*'
@@ -10,6 +12,17 @@ def sanitize_name(name):
     for char in invalid_chars:
         new_name = new_name.replace(char, "")
     return new_name
+
+def get_excel_filename():
+    # 사용자로부터 파일 이름 입력 받기
+    file_name = input("Excel 파일의 이름을 입력하세요 (확장자 .xlsx 포함 가능): ")
+    
+    # 파일 이름에서 .xlsx 확장자 제거
+    if file_name.endswith('.xlsx'):
+        # .xlsx 확장자가 있으면 제거
+        file_name = file_name[:-5]
+    
+    return file_name
 
 def create_unique_directory(base_path, dir_name):
     try:
@@ -27,71 +40,92 @@ def create_unique_directory(base_path, dir_name):
                 new_directory_path = os.path.join(base_path, new_dir_name)
             
             os.mkdir(new_directory_path)
-            print(f"Directory created at: {new_directory_path}")
+            print(f"폴더가 생성되었습니다: {new_directory_path}")
         else:
             os.mkdir(directory_path)
-            print(f"Directory created at: {directory_path}")
+            print(f"폴더가 생성되었습니다:  {directory_path}")
         
         return new_dir_name
-    except ValueError:
+    except Exception as e:
+        print("사용중 오류가 발생했습니다. 하단의 에러 메세지를 개발자에게 전달하여 문제를 해결할 수 있습니다.")
+        print(f"{e}")
         return None
 
 def main():
-    excel_file_path = "./excel.xlsx"
-    df = pd.read_excel(excel_file_path)
-    template_hwp_path = "./template.hwp"
-    file_root = os.path.abspath(os.path.join(os.path.dirname(__file__)))
-    fields = ["name", "number", "artist", "date", "size", "framesize", "material"]
 
-    if os.path.exists(os.path.join(file_root, template_hwp_path)) is False:
-        print("template.hwp파일이 존재하지 않습니다. template.hwp을 해당 프로그램 위치에 배치하여 다시 시도해주세요.")
+  template_hwp_path = "./template.hwp"
+  file_root = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+  fields = ["name", "number", "artist", "date", "size", "framesize", "material"]
 
-        return
+  if os.path.exists(os.path.join(file_root, template_hwp_path)) is False:
+    print("template.hwp이 존재하지 않습니다. template.hwp을 해당 프로그램 위치에 배치하여 다시 시도해주세요.")
 
-    title = input("사업명을 입력해주세요: ")
-    dir_name = create_unique_directory(file_root, title)
+    return
 
-    if dir_name is None:
-        print("사업명이 폴더명에 적합하지 않습니다. 다시 시도해주세요.")
-        
-        return
+  title = input("사업명을 입력해주세요: ")
+  dir_name = create_unique_directory(file_root, title)
 
-    failed_name = []
-
-    for index, row in df.iterrows():
-        if row["name"] is None:
-            print("not name")
-        else:
-            try:
-                file_name = sanitize_name(row["name"])
-                new_file_path = f"./{dir_name}/{file_name}.hwp"
-
-                shutil.copy(template_hwp_path, new_file_path)
-
-                hwp = win32.gencache.EnsureDispatch("Hwpframe.hwpobject")
-                hwp.RegisterModule("FilePathCheckDLL", "FilePathCheckerModule")
-
-                file_path = file_root + new_file_path
-
-                hwp.Open(file_path)
-                
-                hwp.PutFieldText("title", title)
-
-                for field in fields:
-                    if field in df.columns: 
-                        if row[field] is None:
-                            hwp.PutFieldText(field, "-")
-                        else:
-                            hwp.PutFieldText(field, str(row[field]))
-                    else:
-                        hwp.PutFieldText(field, "-")
-
-                hwp.Save()
-                hwp.Quit()
-            except ValueError:
-                failed_name.push(row["name"])
+  if dir_name is None:
+    print("사업명이 폴더명에 적합하지 않습니다. 다시 시도해주세요.")
     
-    print("실패한 그림 목록입니다.")
-    print(failed_name.join(", "))
+    return
+  
+  excel_file_name = get_excel_filename()
+  excel_file_path = f"./{excel_file_name}.xlsx"
 
-main()
+  if os.path.exists(os.path.join(file_root, excel_file_path)) is False:
+    print(f"{excel_file_name}.xlsx이 존재하지 않습니다. {excel_file_name}.xlsx을 해당 프로그램 위치에 배치하여 다시 시도해주세요.")
+
+    return
+
+  df = pd.read_excel(excel_file_path)
+
+  failed_names = []
+
+  for index, row in tqdm(df.iterrows(), total=df.shape[0], desc="진행 중"):
+    if keyboard.is_pressed('esc'):
+      response = input("작업을 중단하시겠습니까? (y/n): ")
+      if response.lower() == 'y':
+          print("작업이 중단되었습니다.")
+          break
+
+    if row["name"] is None:
+        print("not name")
+    else:
+      try:
+        file_name = sanitize_name(row["name"])
+        new_file_path = f"./{dir_name}/{index}-{file_name}.hwp"
+
+        shutil.copy(template_hwp_path, new_file_path)
+
+        hwp = win32.gencache.EnsureDispatch("Hwpframe.hwpobject")
+        hwp.RegisterModule("FilePathCheckDLL", "FilePathCheckerModule")
+
+        file_path = file_root + new_file_path
+
+        hwp.Open(file_path)
+        
+        hwp.PutFieldText("title", title)
+
+        for field in fields:
+          if field in df.columns: 
+            if row[field] is None:
+                hwp.PutFieldText(field, "-")
+            else:
+                hwp.PutFieldText(field, str(row[field]))
+          else:
+              hwp.PutFieldText(field, "-")
+
+        hwp.Save()
+        hwp.Quit()
+      except ValueError:
+          failed_names.append(f"[row-index: {index}, name: {row["name"]}]")
+  
+  print("실패한 그림 목록입니다.")
+  print(", ".join(failed_names))
+
+try:
+  main()
+except Exception as e:
+  print("사용중 오류가 발생했습니다. 하단의 에러 메세지를 개발자에게 전달하여 문제를 해결할 수 있습니다.")
+  print(f"{e}")
